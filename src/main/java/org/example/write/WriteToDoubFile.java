bc72daa2627cb17602799cd2b918e00e70f8d964
Vladimir Kolontsov <kolontsovvova@mail.ru>
2024-09-17 10:02:01 +0700

    finish

--- src/main/java/org/example/write/WriteToDoubFile.java
+++ src/main/java/org/example/write/WriteToDoubFile.java
@@ -2,6 +2,7 @@ package org.example.write;
 
 import org.example.Main;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -12,8 +13,13 @@ public class WriteToDoubFile {
         // указываем путь к файлу
         String filePath = Main.filePathToWriteDouble;
 
-        try (FileWriter writer = new FileWriter(filePath, true)) {
+        try {
+            File file = new File(filePath);
+            file.getParentFile().mkdirs();
+
+            FileWriter writer = new FileWriter(file, true);
             writer.write(data + "\n");
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/org/example/write/WriteToStrFile.java
+++ src/main/java/org/example/write/WriteToStrFile.java
@@ -2,6 +2,7 @@ package org.example.write;
 
 import org.example.Main;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -12,8 +13,13 @@ public class WriteToStrFile {
         // указываем путь к файлу
         String filePath = Main.filePathToWriteString;
 
-        try (FileWriter writer = new FileWriter(filePath, true)) {
+        try {
+            File file = new File(filePath);
+            file.getParentFile().mkdirs();
+
+            FileWriter writer = new FileWriter(file, true);
             writer.write(data + "\n");
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
